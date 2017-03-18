8d61bd59c98ed736d87bcade7239e425d7c7a515
Aditi Ramaswamy <aramas@umich.edu>
2017-03-18 13:32:27 -0400

    fixed power test lol

--- test_rpn.py
+++ test_rpn.py
@@ -7,6 +7,6 @@ class TestBasics(unittest.TestCase):
 	def test_subtract(self):
 		result = rpn.calculate('5 3 -')
 		self.assertEqual(2, result)
-        def test_subtract(self):
+        def test_carat(self):
                 result = rpn.calculate('5 2 ^')
                 self.assertEqual(25, result)
