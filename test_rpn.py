870ceb2c16c2c720751be1648b1b0e6eccdf3350
Aditi Ramaswamy <aramas@umich.edu>
2017-03-18 15:37:38 -0400

    spacing in test

--- test_rpn.py
+++ test_rpn.py
@@ -4,11 +4,11 @@ class TestBasics(unittest.TestCase):
 	def test_add(self):
 		result = rpn.calculate('1 1 +')
 		self.assertEqual(2, result)
-                print("Addition passed\n")
+		print("Addition passed\n")
 	def test_subtract(self):
 		result = rpn.calculate('5 3 -')
 		self.assertEqual(2, result)
-                print("Subtraction passed\n")
+		print("Subtraction passed\n")
 #        def test_carat(self):
 #                result = rpn.calculate('5 2 ^')
 #                self.assertEqual(25, result)
