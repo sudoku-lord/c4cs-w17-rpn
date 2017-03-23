ad7687c0467e14b29f10f1e40742661bb178598b
Aditi Ramaswamy <aramas@umich.edu>
2017-03-23 11:59:23 -0400

    yO

--- test_rpn.py
+++ test_rpn.py
@@ -4,12 +4,12 @@ class TestBasics(unittest.TestCase):
 	def test_add(self):
 		result = rpn.calculate('1 1 +')
 		self.assertEqual(2, result)
-		print("Addition passed\n")
+		print("Addition passed YO\n")
 	def test_subtract(self):
 		result = rpn.calculate('5 3 -')
 		self.assertEqual(2, result)
-		print("Subtraction passed\n")
+		print("Subtraction passed YO YO\n")
 	def test_carat(self):
 		result = rpn.calculate('5 2 ^')
 		self.assertEqual(25, result)
-		print("Exponentiation passed\n")
+		print("Exponentiation passed YO YO YO\n")
