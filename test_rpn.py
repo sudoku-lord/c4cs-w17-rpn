419e6622e85f291edd89ae5d8a5614eb56aa5459
Aditi Ramaswamy <aramas@umich.edu>
2017-03-23 13:54:35 -0400

    re-added colorama, god help me

--- test_rpn.py
+++ test_rpn.py
@@ -1,3 +1,6 @@
+from colorama import init
+init()
+from colorama import Fore, Back, Style
 import unittest
 import rpn
 class TestBasics(unittest.TestCase):
@@ -20,14 +23,14 @@ class TestBasics(unittest.TestCase):
 print("Testing Addition:\n")
 result_one = rpn.calculate('1 1 +')
 if result_one == 2:
-	print("Addition passed YO\n")
+	print(Fore.RED + "Addition passed YO\n")
 
 print("Testing Subtraction:\n")
 result_two = rpn.calculate('5 3 -')
 if result_two == 2:
-	print("Subtraction passed YO YO\n")
+	print(Fore.RED + "Subtraction passed YO YO\n")
 
 print("Testing Exponentiation:\n")
 result_three = rpn.calculate('5 2 ^')
 if result_three == 25:
-	print("Exponentiation passed YO YO YO\n")
+	print(Fore.RED + "Exponentiation passed YO YO YO\n")
