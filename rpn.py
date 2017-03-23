d250270bafd2ef6a8efeeaba82455ff0b027cddd
Aditi Ramaswamy <aramas@umich.edu>
2017-03-23 10:32:39 -0400

    added colorama

--- rpn.py
+++ rpn.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import operator
+from colorama import Fore, Back, Style
 OPERATORS = {
 	'+': operator.add,
 	'-': operator.sub,
@@ -24,6 +25,6 @@ def calculate(arg):
 def main():
 	while True:
 		result = calculate(input('rpn calc> '))
-		print("Result:", result)
+		print(Fore.RED + "Result:", result)
 if __name__ == '__main__':
 	main()
