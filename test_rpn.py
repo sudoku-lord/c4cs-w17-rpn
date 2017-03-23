b9b29fd71dc73508e8cc5d996f6be93ada507a6c
Aditi Ramaswamy <aramas@umich.edu>
2017-03-23 12:11:35 -0400

    added extra print statement

--- test_rpn.py
+++ test_rpn.py
@@ -2,14 +2,17 @@ import unittest
 import rpn
 class TestBasics(unittest.TestCase):
 	def test_add(self):
+		print("Testing Addition:\n")
 		result = rpn.calculate('1 1 +')
 		self.assertEqual(2, result)
 		print("Addition passed YO\n")
 	def test_subtract(self):
+		print("Testing Subtraction:\n")
 		result = rpn.calculate('5 3 -')
 		self.assertEqual(2, result)
 		print("Subtraction passed YO YO\n")
 	def test_carat(self):
+		print("Testing Exponentiation:\n")
 		result = rpn.calculate('5 2 ^')
 		self.assertEqual(25, result)
 		print("Exponentiation passed YO YO YO\n")
